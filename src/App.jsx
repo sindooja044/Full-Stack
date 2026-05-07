c0c71653e0798c11d9702c7cf3ff413f925ef25f
sindooja044 <aaeer015@gmail.com>
2026-05-07 16:15:51 +0530

    added toggle task functionality

--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,27 @@ const App = () => {
 
     }
    const deleteTask=async (id)=>{
-    await fetch('http://localhost:5000/api/tasks',{
+    await fetch(`http://localhost:5000/api/tasks/${id}`,{
             method:"DELETE",
             
 
         });
         setTasks(tasks.filter((t)=>t._id!==id))
    }
+   const toggleTask=async (id)=>{
+   const res=await fetch(`http://localhost:5000/api/tasks/${id}`,{
+            method:"PATCH",
+            
+
+        });
+     const updatedTask= await res.json();
+     setTasks(tasks.map((task)=>task._id===id?updatedTask : task))
+
+   }
   return (
     <div>
       <Taskform addTask={addTask} />
-      <Tasklist  tasks={tasks} deleteTask={deleteTask}/>
+      <Tasklist  tasks={tasks} deleteTask={deleteTask} toggleTask={toggleTask}/>
     </div>
   )
 }
--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-const Tasklist = ({tasks,deleteTask}) => {
+const Tasklist = ({tasks,deleteTask,toggleTask}) => {
   return (
     <div>
       {tasks.map((task)=><ul>
-        <li key={task._id}>{task.title}</li><button onClick={()=>{deleteTask(task._id)}}>DELETE</button></ul>)}
+        <li key={task._id}><button onClick={()=>{toggleTask(task._id)}}>{task.completed? "check":"uncheck"}</button> {task.title}</li><button onClick={()=>{deleteTask(task._id)}}>DELETE</button></ul>)}
     </div>
   )
 }
